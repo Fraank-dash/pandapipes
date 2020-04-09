1b0c6575a966c7593d90cd97bbcc6976708d7b18
Fraank-dash <61389166+fraank-dash@users.noreply.github.com>
2020-04-09 14:00:50 +0200

    Extra-File with Variables to load the Property Library properly

---
+++ pandapipes/properties/carbondioxid/Library_Variables.py
@@ -0,0 +1,15 @@
+def lib_path()
+    Str_FilePath2Use = r"C:\Users\FraankPC\Documents\MasterthesisPanda\pandapipes-pt\pandapipes\pandapipes\properties\carbondioxid\CarbonDioxide.xlsx"
+    return Str_FilePath2Use
+
+
+def lib_tables()
+    StrL_Sheets2Use = ["263K", "273K", "283K", "293K", "295K", "297K", "299K", "301K", "302K", "303K", "304K",
+                       "305K", "307K", "309K", "311K", "313K", "323K", "333K", "343K", "353K", "363K", "373K",
+                       "383K", "393K", "403K", "413K", "423K", "433K", "443K", "453K", "463K", "473K"]
+    return StrL_Sheets2Use
+
+
+def lib_columns()
+    Str_Col2Use = "A,C,D,J,M,N"
+    return Str_Col2Use
