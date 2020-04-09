e9962aa69f0a2447b701771981e089071e744f65
Fraank-dash <61389166+fraank-dash@users.noreply.github.com>
2020-04-09 14:50:27 +0200

    testing stuff...

--- pandapipes/properties/carbondioxid/Library_Test.py
+++ pandapipes/properties/carbondioxid/Library_Test.py
@@ -1,11 +1,11 @@
-import pandapipes.properties.carbondioxid.CarbonDioxide as CO2
-import pandapipes.properties.carbondioxid.Library_Variables as LV
+from pandapipes.properties.carbondioxid import CarbonDioxide as CO2
+from pandapipes.properties.carbondioxid import Library_Variables as LV
 
-path = LV.lib_path
-tables = LV.lib_tables
-columns = LV.lib_columns
+lib_path = LV.STR_FILEPATH2USE
+lib_tables = LV.STRL_SHEETS2USE
+lib_columns = LV.STR_COL2USE
 
-database = CO2.load_property_library(path=path, tables=tables, columns=columns)
+database = CO2.load_property_library(path=lib_path, tables=lib_tables, columns=lib_columns)
 
 Pressure = 180
 'bar'
@@ -13,3 +13,10 @@ Temperature = 300
 'Kelvin'
 
 Interpolated_Values = CO2.interpolate_property(p=Pressure, t=Temperature, property_library=database)
+
+Interpolated_Pressure = Interpolated_Values["Pressure (bar)"]
+Interpolated_Temperature = Interpolated_Values["Temperature (K)"]
+Interpolated_Density = Interpolated_Values["Density (kg/m3)"]
+Interpolated_Cp = Interpolated_Values["Cp (J/g*K)"]
+Interpolated_Viscosity = Interpolated_Values["Viscosity (Pa*s)"]
+Interpolated_ThermCond = Interpolated_Values["Therm. Cond. (W/m*K)"]
